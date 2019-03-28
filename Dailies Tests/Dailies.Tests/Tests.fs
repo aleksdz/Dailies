9054b88a828581e19485ae74821e897c2c4b9461
Aleks <zaersx@gmail.com>
2019-03-28 22:30:02 +0000

    Function for determining any list pairs sum to K

--- Dailies Tests/Dailies.Tests/Tests.fs
+++ Dailies Tests/Dailies.Tests/Tests.fs
@@ -7,3 +7,13 @@ open Dailies
 [<Fact>]
 let ``First Test`` () =
     Assert.Equal(1, Daily.FirstSolution ())
+
+// Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+[<Fact>]
+let ``Any List Tuple Sum Equals K`` () =
+    let numbers = [10;15;3;7]
+    let k = 17
+
+    let doesSumEqualK = Daily.SumListTuplesEqualK numbers k
+
+    Assert.Equal(true, doesSumEqualK)
--- Dailies/Library.fs
+++ Dailies/Library.fs
@@ -4,3 +4,10 @@ module Daily =
 
     let FirstSolution () =
         1
+
+    let SumListTuplesEqualK (numbers : List<int>) (k : int) =
+        let numbersMinusK = List.collect (fun x -> [abs(x - k)]) numbers
+
+        let intersection = Set.intersect (Set.ofList numbers) (Set.ofList numbersMinusK)
+
+        Operators.not intersection.IsEmpty
