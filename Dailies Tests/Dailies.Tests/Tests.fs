02276faac1264db59f8d150b6c2c3c440eaea8f5
Aleks <zaersx@gmail.com>
2019-03-29 19:23:11 +0000

    Hashset implementation O(n)

--- Dailies Tests/Dailies.Tests/Tests.fs
+++ Dailies Tests/Dailies.Tests/Tests.fs
@@ -11,17 +11,17 @@ let ``First Test`` () =
 // Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 let listTupleTestCases =
     seq {
-        yield [|[10;15;3;7] :> obj; 17 :> obj; true    :> obj|]
-        yield [|[10;15;3;7] :> obj; 16 :> obj; false   :> obj|]
-        yield [|[1;2;3;4]   :> obj; 3  :> obj; true    :> obj|]
-        yield [|[1;2;3;4]   :> obj; 1  :> obj; false   :> obj|]
-        yield [|[0;0;0;0]   :> obj;  0 :> obj; true    :> obj|]
-        yield [|[12312314;56754652;324356;85735246]    :> obj; 16   :> obj; false   :> obj|]
+        yield [| Collections.Generic.List([10;15;3;7]) :> obj; 17 :> obj; true    :> obj|]
+        yield [| Collections.Generic.List([10;15;3;7]) :> obj; 16 :> obj; false   :> obj|]
+        yield [| Collections.Generic.List([1;2;3;4])   :> obj; 3  :> obj; true    :> obj|]
+        yield [| Collections.Generic.List([1;2;3;4])   :> obj; 1  :> obj; false   :> obj|]
+        yield [| Collections.Generic.List([0;0;0;0])   :> obj; 0  :> obj; true    :> obj|]
+        yield [| Collections.Generic.List([12312314;56754652;324356;85735246])    :> obj; 16   :> obj; false   :> obj|]
     }
 
 [<Theory>]
 [<MemberData("listTupleTestCases")>]
-let ``Any List Tuple Sum Equals K`` (numbers : List<int>) (k : int) (expectedResult : bool) =
+let ``Any List Tuple Sum Equals K`` (numbers : Collections.Generic.List<int>) (k : int) (expectedResult : bool) =
     let doesSumEqualK = Daily.SumListTuplesEqualK numbers k
 
     Assert.Equal(expectedResult, doesSumEqualK)
--- Dailies/Library.fs
+++ Dailies/Library.fs
@@ -1,11 +1,12 @@
 namespace Dailies
 
 module Daily =
+    open System.Collections.Generic
+    open System.Linq
 
     let FirstSolution () =
         1
 
     let SumListTuplesEqualK (numbers : List<int>) (k : int) =
-        let tuplesSummed = List.collect (fun x -> List.collect (fun y -> [y + x]) numbers) numbers
-
-        List.contains k tuplesSummed
+        let hashset = HashSet numbers 
+        hashset.Any(fun element -> hashset.Contains(k - element))
